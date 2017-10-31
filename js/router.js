3a86d9ae99677c9d855576ee357ee117ac0822f3
rudmanmrrod <rudman22@hotmail.com>
2017-10-31 08:28:15 -0400

    Agregada estructura inicial de la presentación

---
+++ js/router.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	cargar_contenido('#1_int','1_portada.html');
+	cargar_contenido('#2_ter','2_terminos.html');
+	cargar_contenido('#3_scp','3_scp.html');
+	cargar_contenido('#4_mod','4_modulos.html');
+	cargar_contenido('#5_func_lda','5_funcionamiento_lda.html');
+	cargar_contenido('#6_pant_part','6_pantalla_participacion.html');
+	cargar_contenido('#7_pant_gest','7_pantalla_gestor.html');
+	cargar_contenido('#8_pant_proc','8_pantalla_procesamiento.html');
+	cargar_contenido('#9_nueva_estruc','9_nueva_estructura.html');
+	cargar_contenido('#10_pant_nv','10_sistemas.html');
+});
